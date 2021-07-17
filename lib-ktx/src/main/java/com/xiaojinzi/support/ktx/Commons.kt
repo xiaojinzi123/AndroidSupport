ad17e9509d892eeb3049998fc6bf875137557303
xiaojinzi <cxj0xiaojinzi@gmail.com>
2021-07-17 17:15:32 +0800

    feat: 增加 ktx

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Commons.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Commons.kt
@@ -1,6 +1,7 @@
 package com.xiaojinzi.support.ktx
 
 import android.app.Application
+import android.view.View
 import com.xiaojinzi.support.init.AppInstance
 import java.util.*
 
@@ -14,3 +15,6 @@ val app: Application
  * 创建一个 UUID
  */
 fun newUUid() = UUID.randomUUID().toString()
+
+inline fun Boolean.toVisible(isGone: Boolean = false) =
+    if (this) View.VISIBLE else (if (isGone) View.GONE else View.INVISIBLE)
