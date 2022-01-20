b3a515830ae855b39239c481a85ca374c824b930
xiaojinzi <cxj0xiaojinzi@gmail.com>
2022-01-20 17:01:09 +0800

    feat: 增加一个扩展

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Coroutines.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Coroutines.kt
@@ -1,7 +1,9 @@
 package com.xiaojinzi.support.ktx
 
 import com.xiaojinzi.support.init.AppInstance
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Job
 
 /**
  * 忽略错误的一个协程的 Context
@@ -12,3 +14,7 @@ val ErrorIgnoreContext: CoroutineExceptionHandler
             throwable.printStackTrace()
         }
     }
+
+fun List<Job>.cancelAll(cause: CancellationException? = null) {
+    this.forEach { it.cancel(cause = cause) }
+}
