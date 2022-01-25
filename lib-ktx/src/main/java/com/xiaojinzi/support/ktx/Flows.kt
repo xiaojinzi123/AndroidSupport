df54cfcba4c1d94ccde98b69ba1f663f51389b2a
xiaojinzi <cxj0xiaojinzi@gmail.com>
2022-01-25 09:33:18 +0800

    feat: 增加 Flow 一个 takeUntil 的操作符

---
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Flows.kt
@@ -0,0 +1,59 @@
+package com.xiaojinzi.support.ktx
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.AbstractFlow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.collect
+
+@FlowPreview
+fun <T> Flow<T>.takeUntil(
+    isIncludeLastOne: Boolean = true,
+    condition: (value: T) -> Boolean
+): Flow<T> {
+    return FlowTakeUntil(
+        source = this,
+        condition = condition,
+        isIncludeLastOne = isIncludeLastOne,
+    )
+}
+
+@FlowPreview
+internal class FlowTakeUntil<T>(
+    private val source: Flow<T>,
+    private val condition: (value: T) -> Boolean,
+    private val isIncludeLastOne: Boolean = true,
+) : AbstractFlow<T>() {
+
+    @InternalCoroutinesApi
+    override suspend fun collectSafely(collector: FlowCollector<T>) {
+
+        coroutineScope {
+            try {
+                source.collect {
+                    if (condition.invoke(it)) {
+                        if (isIncludeLastOne) {
+                            collector.emit(value = it)
+                        }
+                        throw STOP
+                    }
+                    collector.emit(value = it)
+                }
+            } catch (e: StopException) {
+                // ok
+            }
+
+        }
+
+    }
+
+    class StopException : CancellationException()
+
+    companion object {
+        val STOP = StopException()
+    }
+
+}
