c0488fe3aef8efab1c0c31ade167d34853bce2e6
xiaojiniz <347837667@qq.com>
2022-03-18 15:29:43 +0800

    升级 AGP

--- app/src/main/java/com/xiaojinzi/support/demo/App.kt
+++ app/src/main/java/com/xiaojinzi/support/demo/App.kt
@@ -2,7 +2,6 @@ package com.xiaojinzi.support.demo
 
 import android.app.Application
 import android.content.Context
-import androidx.multidex.MultiDex
 import com.xiaojinzi.component.Component
 import com.xiaojinzi.component.Config
 import com.xiaojinzi.support.init.AppInstance
@@ -21,7 +20,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        MultiDex.install(this);
 
         Component.init(
             BuildConfig.DEBUG,
