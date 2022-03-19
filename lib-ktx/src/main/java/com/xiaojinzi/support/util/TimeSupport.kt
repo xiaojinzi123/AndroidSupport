7dc2a6855f18b718db3b138b624a5f5cecc621c7
xiaojiniz <347837667@qq.com>
2022-03-19 17:10:47 +0800

    纠正类型

--- lib-ktx/src/main/java/com/xiaojinzi/support/util/TimeSupport.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/util/TimeSupport.kt
@@ -3,19 +3,19 @@ package com.xiaojinzi.support.util
 /**
  * 一秒的毫秒值
  */
-const val SECOND_MS = 1000
+const val SECOND_MS: Long = 1000L
 
 /**
  * 一分钟的毫秒值
  */
-const val MINUTE_MS = 60 * SECOND_MS
+const val MINUTE_MS: Long = 60 * SECOND_MS
 
 /**
  * 一小时的毫秒值
  */
-const val HOUR_MS = 60 * MINUTE_MS
+const val HOUR_MS: Long = 60 * MINUTE_MS
 
 /**
  * 一天的毫秒值
  */
-const val DAY_MS = 24 * HOUR_MS
+const val DAY_MS: Long = 24 * HOUR_MS
