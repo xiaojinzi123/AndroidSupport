01c9332cf9faabf37bfe57d02e53ef4555b6f854
xiaojiniz <347837667@qq.com>
2022-03-19 16:57:49 +0800

    增加一个时间工具类

---
+++ lib-ktx/src/main/java/com/xiaojinzi/support/util/TimeSupport.kt
@@ -0,0 +1,21 @@
+package com.xiaojinzi.support.util
+
+/**
+ * 一秒的毫秒值
+ */
+const val SECOND_MS = 1000
+
+/**
+ * 一分钟的毫秒值
+ */
+const val MINUTE_MS = 60 * SECOND_MS
+
+/**
+ * 一小时的毫秒值
+ */
+const val HOUR_MS = 60 * MINUTE_MS
+
+/**
+ * 一天的毫秒值
+ */
+const val DAY_MS = 24 * HOUR_MS
