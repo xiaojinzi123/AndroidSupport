4f63cc6ac9ad78f94398f0bc877135e01dba0ca9
xiaojiniz <347837667@qq.com>
2022-04-13 18:09:17 +0800

    feat: 增加扩展 format2f

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Numbers.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Numbers.kt
@@ -1,7 +1,15 @@
 package com.xiaojinzi.support.ktx
 
+fun Float.format1f(): String {
+    return String.format("%.1f", this)
+}
+
 fun Float.format2f(): String {
     return String.format("%.2f", this)
+}
+
+fun Double.format1f(): String {
+    return String.format("%.1f", this)
 }
 
 fun Double.format2f(): String {
