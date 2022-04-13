f944e3d027c268700ad63acc0810ebeb084ab95c
xiaojiniz <347837667@qq.com>
2022-04-13 14:24:59 +0800

    feat: 增加扩展 clickableNoRipple

--- lib-compose/src/main/java/com/xiaojinzi/support/compose/util/Modifiers.kt
+++ lib-compose/src/main/java/com/xiaojinzi/support/compose/util/Modifiers.kt
@@ -1,9 +1,36 @@
 package com.xiaojinzi.support.compose.util
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.semantics.Role
 
+/**
+ * 圆形裁剪
+ */
 fun Modifier.circleClip(): Modifier {
     return this.clip(shape = CircleShape)
+}
+
+/**
+ * 没有涟漪效果的点击
+ */
+fun Modifier.clickableNoRipple(
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit,
+) = composed {
+    Modifier.clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() },
+        enabled = enabled,
+        onClickLabel = onClickLabel,
+        role = role,
+        onClick = onClick,
+    )
 }
