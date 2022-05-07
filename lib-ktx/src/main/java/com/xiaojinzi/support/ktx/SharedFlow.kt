811347cdeb0d0ac2e293d0ff5d4238f20384620c
xiaojiniz <347837667@qq.com>
2022-05-07 16:05:33 +0800

    feat: SharedFlow.kt 增加扩展

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/SharedFlow.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/SharedFlow.kt
@@ -1,7 +1,12 @@
 package com.xiaojinzi.support.ktx
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @Suppress("FunctionName", "UNCHECKED_CAST")
 fun <T> NormalMutableSharedFlow() = MutableSharedFlow<T>(
@@ -9,3 +14,34 @@ fun <T> NormalMutableSharedFlow() = MutableSharedFlow<T>(
     extraBufferCapacity = 1,
     onBufferOverflow = BufferOverflow.SUSPEND,
 )
+
+private fun <T> Flow<T>.doSharedIn(
+    shareFlow: MutableSharedFlow<T>,
+    scope: CoroutineScope,
+) {
+    val upstream = this
+    scope.launch {
+        upstream.collect {
+            shareFlow.emit(value = it)
+        }
+    }
+}
+
+fun <T> Flow<T>.mutableSharedIn(
+    scope: CoroutineScope,
+): MutableSharedFlow<T> {
+    val shareFlow = MutableSharedFlow<T>()
+    this.doSharedIn(
+        shareFlow = shareFlow,
+        scope = scope,
+    )
+    return shareFlow
+}
+
+fun <T> Flow<T>.sharedIn(
+    scope: CoroutineScope,
+): SharedFlow<T> {
+    return this.mutableSharedIn(
+        scope = scope,
+    )
+}
