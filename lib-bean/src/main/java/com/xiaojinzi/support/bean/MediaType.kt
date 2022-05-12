873ca0f1c1cc48c2b9165e93624f9087518303d9
xiaojiniz <347837667@qq.com>
2022-05-12 15:25:45 +0800

    feat: 增加 MediaType

---
+++ lib-bean/src/main/java/com/xiaojinzi/support/bean/MediaType.kt
@@ -0,0 +1,16 @@
+package com.xiaojinzi.support.bean
+
+/**
+ * 待更新
+ */
+object MediaType {
+
+    const val VIDEO_MP4 = "video/mp4"
+
+    const val AUDIO_MP3 = "audio/mp3"
+
+    const val APPLICATION_JSON = "application/json"
+
+    const val TEXT_HTML = "text/html"
+
+}
