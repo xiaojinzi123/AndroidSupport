fc352d7183fd939873cff100258f638e4b804e58
xiaojiniz <347837667@qq.com>
2022-05-14 14:55:38 +0800

    feat: 增加扩展

--- lib-compose/src/main/java/com/xiaojinzi/support/compose/util/Commons.kt
+++ lib-compose/src/main/java/com/xiaojinzi/support/compose/util/Commons.kt
@@ -1,10 +1,25 @@
 package com.xiaojinzi.support.compose.util
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import com.xiaojinzi.support.bean.StringItemDto
 
 @Composable
 fun StringItemDto.contentWithComposable(): String {
     return value ?: stringResource(id = valueRsd!!)
+}
+
+@Composable
+fun Float.toDp(): Dp {
+    val value = this
+    return with(LocalDensity.current) {
+        value.toDp()
+    }
+}
+
+@Composable
+fun Int.toDp(): Dp {
+    return this.toFloat().toDp()
 }
