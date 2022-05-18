db117b752794f164da45868e419c5f8073831f2b
xiaojiniz <347837667@qq.com>
2022-05-18 09:24:06 +0800

    feat: 增加扩展

--- app/src/main/java/com/xiaojinzi/support/demo/MainAct.kt
+++ app/src/main/java/com/xiaojinzi/support/demo/MainAct.kt
@@ -42,7 +42,6 @@ class MainAct : AppCompatActivity() {
             testContent()
         }
 
-
     }
 
 }
---
+++ lib-annotation/src/main/java/com/xiaojinzi/support/annotation/Values.kt
@@ -0,0 +1,107 @@
+package com.xiaojinzi.support.annotation
+
+/**
+ * 表示一个时间
+ */
+@Retention(value = AnnotationRetention.SOURCE)
+@Target(
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.TYPE_PARAMETER,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.LOCAL_VARIABLE,
+)
+annotation class TimeValue(
+
+    /**
+     * 值的类型
+     */
+    val value: Type
+
+) {
+
+    enum class Type {
+
+        /**
+         * 毫秒值
+         */
+        MILLISECOND,
+
+        /**
+         * 秒
+         */
+        SECOND,
+
+        /**
+         * 分钟
+         */
+        MINUTE,
+
+        /**
+         * 小时
+         */
+        HOUR,
+
+        /**
+         * 天
+         */
+        DAY,
+
+    }
+
+}
+
+/**
+ * 表示一个文件大小的值
+ */
+@Retention(value = AnnotationRetention.SOURCE)
+@Target(
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.TYPE_PARAMETER,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.LOCAL_VARIABLE,
+)
+annotation class FileSizeValue(
+
+    /**
+     * 值的类型
+     */
+    val value: Type
+
+) {
+
+    enum class Type {
+
+        /**
+         * 字节
+         */
+        BYTE,
+
+        /**
+         * KB
+         */
+        KB,
+
+        /**
+         * MB
+         */
+        MB,
+
+        /**
+         * GB
+         */
+        GB,
+
+        /**
+         * TB
+         */
+        TB,
+
+    }
+
+}
