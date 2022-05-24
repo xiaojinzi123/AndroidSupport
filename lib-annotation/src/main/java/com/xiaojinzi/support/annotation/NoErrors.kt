8d3f9df42c11593b85fc77ff6a098726c2d13b5b
xiaojiniz <347837667@qq.com>
2022-05-24 17:11:35 +0800

    feat: 优化扩展2

--- lib-annotation/src/main/java/com/xiaojinzi/support/annotation/NoErrors.kt
+++ lib-annotation/src/main/java/com/xiaojinzi/support/annotation/NoErrors.kt
@@ -1,6 +1,15 @@
 package com.xiaojinzi.support.annotation
 
 /**
+ * 表示不会有错误发生的方法
+ */
+@Retention(value = AnnotationRetention.SOURCE)
+@Target(
+    AnnotationTarget.FUNCTION
+)
+annotation class NoError
+
+/**
  * 表示不会有错误发生的 Observable(可观察者对象)
  */
 @Retention(value = AnnotationRetention.SOURCE)
