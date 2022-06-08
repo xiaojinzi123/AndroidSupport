b4b01e005c12b4c0abf54e5def04f9131c9be8c2
xiaojinzi <cxj0xiaojinzi@gmail.com>
2022-06-08 22:13:50 +0800

    feat: EventPublisher 的编写

--- lib-ktx/src/main/java/com/xiaojinzi/support/util/EventPublisher.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/util/EventPublisher.kt
@@ -2,17 +2,21 @@ package com.xiaojinzi.support.util
 
 import com.xiaojinzi.support.ktx.NormalMutableSharedFlow
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 
+/**
+ * 事件的载体
+ */
 sealed class EventDto
 
 /**
  * 全局事件分发
  */
-object Event {
+object EventPublisher {
 
     /**
      * 事件流
      */
-    val eventObservable: Flow<EventDto> = NormalMutableSharedFlow()
+    val eventObservable: MutableSharedFlow<EventDto> = NormalMutableSharedFlow()
 
 }
