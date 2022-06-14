dcd2dd83490fcc649c2feb81d781285b2bc619f7
xiaojiniz <347837667@qq.com>
2022-06-14 16:26:29 +0800

    feat: 优化扩展

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Exceptions.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Exceptions.kt
@@ -3,10 +3,11 @@ package com.xiaojinzi.support.ktx
 /**
  * 获取真实的错误
  */
-fun Throwable.getRealThrowable(throwable: Throwable): Throwable {
-    var throwable = throwable
-    while (throwable.cause != null) {
-        throwable = throwable.cause!!
+val Throwable.realThrowable: Throwable
+    get() {
+        var throwable = this
+        while (throwable.cause != null) {
+            throwable = throwable.cause!!
+        }
+        return throwable
     }
-    return throwable
-}
