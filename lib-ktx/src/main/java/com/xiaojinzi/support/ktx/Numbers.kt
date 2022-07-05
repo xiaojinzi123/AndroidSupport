3dc02a4557c475d64e8895d9902777eeb14c3448
xiaojiniz <347837667@qq.com>
2022-07-05 18:52:46 +0800

    feat: 增加扩展

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Numbers.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Numbers.kt
@@ -8,12 +8,20 @@ fun Float.format2f(): String {
     return String.format("%.2f", this)
 }
 
+fun Float.format3f(): String {
+    return String.format("%.3f", this)
+}
+
 fun Double.format1f(): String {
     return String.format("%.1f", this)
 }
 
 fun Double.format2f(): String {
     return String.format("%.2f", this)
+}
+
+fun Double.format3f(): String {
+    return String.format("%.3f", this)
 }
 
 fun Int.toBoolean(): Boolean {
