b37df449b15cc8d82038dae7fc049deb1d869392
xiaojiniz <347837667@qq.com>
2022-11-24 17:32:18 +0800

    feat: 增加 Bitmap 的扩展

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Bitmaps.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Bitmaps.kt
@@ -3,6 +3,21 @@ package com.xiaojinzi.support.ktx
 import android.graphics.Bitmap
 import java.io.File
 
+/**
+ * 可以安全使用的 Bitmap
+ */
+inline fun <R> Bitmap.use(block: (Bitmap) -> R): R {
+    try {
+        return block(this)
+    } finally {
+        try {
+            recycle()
+        } catch (e: Exception) {
+            // ignore
+        }
+    }
+}
+
 @Throws(RuntimeException::class)
 fun Bitmap.writeToFile(targetFile: File): File {
     targetFile.outputStream().use { outputStream ->
