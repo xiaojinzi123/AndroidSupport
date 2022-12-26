f5657e9dbe39d1305e545bf2b85d217a3b703f85
xiaojiniz <347837667@qq.com>
2022-12-26 19:58:18 +0800

    feat: 增加 getImageSizeOrNull

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Medias.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Medias.kt
@@ -18,3 +18,14 @@ fun File.getImageSize(): Pair<Int, Int> {
         options.outWidth to options.outHeight
     }
 }
+
+/**
+ * 获取图片的大小
+ */
+fun File.getImageSizeOrNull(): Pair<Int, Int>? {
+    return try {
+        getImageSize()
+    } catch (e: Exception) {
+        null
+    }
+}
