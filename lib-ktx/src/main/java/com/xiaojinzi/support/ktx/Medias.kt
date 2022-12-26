536133a06257f0176791211e2c182fb408064a90
xiaojiniz <347837667@qq.com>
2022-12-26 19:43:42 +0800

    feat: 增加获取图片的宽高的扩展

---
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Medias.kt
@@ -0,0 +1,20 @@
+package com.xiaojinzi.support.ktx
+
+import android.graphics.BitmapFactory
+import java.io.File
+
+/**
+ * 获取图片的大小
+ */
+fun File.getImageSize(): Pair<Int, Int> {
+    return this.inputStream().use {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeStream(
+            it,
+            null,
+            options,
+        )
+        options.outWidth to options.outHeight
+    }
+}
