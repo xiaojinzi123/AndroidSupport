0ca98db2e966fc70392c8fe0a8316f6a01ac7d33
xiaojiniz <347837667@qq.com>
2023-05-14 14:13:06 +0800

    feat: 优化 joinIgnoreException 方法

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Coroutines.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Coroutines.kt
@@ -1,10 +1,17 @@
 package com.xiaojinzi.support.ktx
 
 import com.xiaojinzi.support.init.AppInstance
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
+
+interface DeferredEmpty : Deferred<Nothing>
 
 /**
  * 忽略错误的一个协程的 Context
@@ -16,9 +23,32 @@ val ErrorIgnoreContext: CoroutineExceptionHandler
         }
     }
 
+suspend fun DeferredEmpty.awaitIgnoreException() {
+    try {
+        this.await()
+    } catch (e: Exception) {
+        // ignore
+    }
+}
+
+fun DeferredEmpty.executeIgnoreException() {
+    val targetDeferred = this
+    AppScope.launch(context = ErrorIgnoreContext) {
+        targetDeferred.awaitIgnoreException()
+    }
+}
+
 /**
  * join 一个 Job, 忽略异常
  */
+@Deprecated(
+    message = "this method is not working, please use awaitIgnoreException()",
+    replaceWith = ReplaceWith(
+        expression = "awaitIgnoreException()",
+        imports = ["com.xiaojinzi.support.ktx.awaitIgnoreException"]
+    ),
+    level = DeprecationLevel.WARNING,
+)
 suspend fun Job.joinIgnoreException() {
     try {
         join()
