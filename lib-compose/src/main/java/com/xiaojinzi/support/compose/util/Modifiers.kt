dc4659719d9ce463371974f5bf6930221f31d7fe
xiaojiniz <347837667@qq.com>
2023-05-28 14:28:06 +0800

    feat: 增加 clickScaleEffect 效果

--- lib-compose/src/main/java/com/xiaojinzi/support/compose/util/Modifiers.kt
+++ lib-compose/src/main/java/com/xiaojinzi/support/compose/util/Modifiers.kt
@@ -1,12 +1,21 @@
 package com.xiaojinzi.support.compose.util
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.semantics.Role
 
 /**
@@ -39,3 +48,32 @@ fun Modifier.clickableNoRipple(
  * 点击事件占位
  */
 fun Modifier.clickPlaceholder() = this.clickableNoRipple(enabled = false) {}
+
+/**
+ * 点击的事件的缩放效果
+ */
+@Composable
+fun Modifier.clickScaleEffect(minScale: Float = 0.8f): Modifier {
+    var scale by remember {
+        mutableStateOf(
+            1f
+        )
+    }
+    val animScale by animateFloatAsState(targetValue = scale, label = "")
+    return this
+        .pointerInput(Unit) {
+            // 等待按下
+            awaitPointerEventScope {
+                while (true) {
+                    awaitFirstDown()
+                    scale = minScale
+                    waitForUpOrCancellation()
+                    scale = 1f
+                }
+            }
+        }
+        .graphicsLayer {
+            this.scaleX = animScale
+            this.scaleY = animScale
+        }
+}
