e5b0443ad1177613491c904a874bc434ac9507a5
xiaojiniz <347837667@qq.com>
2024-01-12 18:51:57 +0800

    feat: dp2sp 的公开

--- lib-compose/src/main/java/com/xiaojinzi/support/compose/util/TextUnits.kt
+++ lib-compose/src/main/java/com/xiaojinzi/support/compose/util/TextUnits.kt
@@ -5,17 +5,17 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
-private val Int.dp2sp: TextUnit
+val Int.dp2sp: TextUnit
     @Composable get() = with(LocalDensity.current) {
         this@dp2sp.dp.toPx().toSp()
     }
 
-private val Float.dp2sp: TextUnit
+val Float.dp2sp: TextUnit
     @Composable get() = with(LocalDensity.current) {
         this@dp2sp.dp.toPx().toSp()
     }
 
-private val Double.dp2sp: TextUnit
+val Double.dp2sp: TextUnit
     @Composable get() = with(LocalDensity.current) {
         this@dp2sp.dp.toPx().toSp()
     }
