971901432a80d7b2d8c01e9287463e514969f89d
xiaojiniz <347837667@qq.com>
2024-04-23 09:48:02 +0800

    优化 format1f 等方法

--- lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Numbers.kt
+++ lib-ktx/src/main/java/com/xiaojinzi/support/ktx/Numbers.kt
@@ -1,27 +1,78 @@
 package com.xiaojinzi.support.ktx
 
-fun Float.format1f(): String {
-    return String.format("%.1f", this)
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+fun Float.format1f(
+    isKeepZero: Boolean = true,
+): String {
+    return if(isKeepZero) {
+        String.format("%.1f", this)
+    } else {
+        DecimalFormat("#.#").apply {
+            this.roundingMode = RoundingMode.HALF_UP
+        }.format(this)
+    }
 }
 
-fun Float.format2f(): String {
-    return String.format("%.2f", this)
+fun Float.format2f(
+    isKeepZero: Boolean = true,
+): String {
+    return if(isKeepZero) {
+        String.format("%.2f", this)
+    } else {
+        DecimalFormat("#.##").apply {
+            this.roundingMode = RoundingMode.HALF_UP
+        }.format(this)
+    }
 }
 
-fun Float.format3f(): String {
-    return String.format("%.3f", this)
+fun Float.format3f(
+    isKeepZero: Boolean = true,
+): String {
+    return if(isKeepZero) {
+        String.format("%.3f", this)
+    } else {
+        DecimalFormat("#.###").apply {
+            this.roundingMode = RoundingMode.HALF_UP
+        }.format(this)
+    }
 }
 
-fun Double.format1f(): String {
-    return String.format("%.1f", this)
+fun Double.format1f(
+    isKeepZero: Boolean = true,
+): String {
+    return if(isKeepZero) {
+        String.format("%.1f", this)
+    } else {
+        DecimalFormat("#.#").apply {
+            this.roundingMode = RoundingMode.HALF_UP
+        }.format(this)
+    }
 }
 
-fun Double.format2f(): String {
-    return String.format("%.2f", this)
+fun Double.format2f(
+    isKeepZero: Boolean = true,
+): String {
+    return if(isKeepZero) {
+        String.format("%.2f", this)
+    } else {
+        DecimalFormat("#.##").apply {
+            this.roundingMode = RoundingMode.HALF_UP
+        }.format(this)
+    }
 }
 
-fun Double.format3f(): String {
-    return String.format("%.3f", this)
+fun Double.format3f(
+    isKeepZero: Boolean = true,
+): String {
+    return if(isKeepZero) {
+        String.format("%.3f", this)
+    } else {
+        DecimalFormat("#.###").apply {
+            this.roundingMode = RoundingMode.HALF_UP
+        }.format(this)
+    }
 }
 
 fun Int.toBoolean(): Boolean {
